ba2b43d5db85e0a17f5596665231e61d0c79b0e1
rs-santos <103576020+rs-santos@users.noreply.github.com>
2023-05-22 16:48:55 -0300

    v1

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>supremos</title>
+</head>
+<body>
+
+           <!-- LOGO E LOGIN-->
+
+    <div class="nome">
+   <img src="img/logo1.png" alt="brasao" width="200px" height="150px">   
+ 
+  
+  
+    </div><br>
+    
+    <!--LIVROS-->
+
+     <fieldset class="conteudo">
+        <img src="img/mr1.jpg" alt="maze runner-correr ou morrer" width="100px"> <p><b>NOME:</b>Maze runner-correr ou morrer</p><BR>
+        <p><b>SINOPSE:</b>  Ao acordar dentro de um escuro elevador em movimento, a única coisa que Thomas consegue lembrar é de seu nome. Sua memória está completamente apagada. Mas ele não está sozinho. Quando a caixa metálica chega a seu destino e as portas se abrem, Thomas se vê rodeado por garotos que o acolhem e o apresentam "A Clareira", um espaço aberto cercado por muros gigantescos. Assim como Thomas, nenhum deles sabe como foi parar ali, nem por quê. Sabem apenas que todas as manhãs as portas de pedra do Labirinto que os cerca se abrem, e, à noite, se fecham. E que a cada trinta dias um novo garoto é entregue pelo elevador. Porém, um fato altera de forma radical a rotina do lugar - chega uma garota, a primeira enviada à Clareira. E mais surpreendente ainda é a mensagem que ela traz consigo. Thomas será mais importante do que imagina, mas para isso terá de descobrir os sombrios segredos guardados em sua mente e correr... correr muito.</p>
+     </fieldset> <br>
+
+     <fieldset class="conteudo">
+        <img src="img/mz2.jpg" alt="maze runner-prova de fogo" width="100px"> <p><b>NOME:</b>Maze runner-prova de fogo</p><BR>
+
+        <p><b>SINOPSE:</b>Depois de superarem os perigos mortais do Labirinto, Thomas e seus amigos acreditam que estão a salvo em uma nova realidade. Mas a aparente tranquilidade é interrompida quando são acordados no meio da noite por gritos lancinantes de criaturas disformes – os Cranks – que ameaçam devorá-los vivos. </p> </fieldset><br>
+
+        
+     <fieldset class="conteudo">
+        <img src="img/mz3.jpg" alt="maze runner-a cura mortal" width="100px"> <p><b>NOME:</b>Maze runner-a cura mortal</p><BR>
+
+        <p><b>SINOPSE:</b>Depois de superarem os perigos mortais do Labirinto, Thomas e seus amigos acreditam que estão a salvo em uma nova realidade. Mas a aparente tranquilidade é interrompida quando são acordados no meio da noite por gritos lancinantes de criaturas disformes – os Cranks – que ameaçam devorá-los vivos. </p> </fieldset><br>
+
+        <!--RODA PÉ-->
+       
+        <!--FIM DO RODA PÉ-->
+
+</body>
+</html>
